b913200252ef45669df42cf0c85ee3c3ce4d9c8f
sasidhar-chavva <chavvasasidhar@gmail.com>
2025-01-30 09:14:57 -0600

    Headshot Static Resource and related changes

--- force-app/main/default/lwc/portfolioHomePage/portfolioHomePage.js
+++ force-app/main/default/lwc/portfolioHomePage/portfolioHomePage.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
+import HEADSHOTS from '@salesforce/resourceUrl/HeadShot';
 
-export default class PortfolioHomePage extends LightningElement {}
+export default class PortfolioHomePage extends LightningElement {
+
+    imageURL = HEADSHOTS + '/My_Headshot.jpg';
+
+}
